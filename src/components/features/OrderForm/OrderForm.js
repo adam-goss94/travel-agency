ea38fc9eb553a21c844fe1a31b3d8d4a1c583388
Adam Goss <adam.goss94@gmail.com>
2020-04-30 18:29:40 +0200

    Add send order functionality

--- src/components/features/OrderForm/OrderForm.js
+++ src/components/features/OrderForm/OrderForm.js
@@ -1,30 +1,83 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import {Row, Col} from 'react-flexbox-grid';
-import pricing from '../../../data/pricing.json';
-
 import OrderSummary from '../OrderSummary/OrderSummary';
 import OrderOption from '../OrderOption/OrderOption';
+// import styles from './OrderForm.scss';
+import { Col, Row } from 'react-flexbox-grid';
+import pricing from '../../../data/pricing.json';
+import PropTypes from 'prop-types';
+import Button from '../../common/Button/Button';
+import { formatPrice } from '../../../utils/formatPrice';
+import { calculateTotal } from '../../../utils/calculateTotal';
+import settings from '../../../data/settings';
 
+const sendOrder = (options, tripCost, tripDays, tripName, tripCountry) => {
+  let orderShouldBeSend = true;
+  console.log(tripDays);
+  (options.contact === '' || !options.name || options['start-date'] === '') ? orderShouldBeSend = false : null;
 
-const OrderForm = ({tripCost, options, setOrderOption}) => (
-  <Row>
-    {pricing.map(option => (
-      <Col md={4} key={option.id}>
-        <OrderOption {...option} currentValue={options[option.id]} setOrderOption={setOrderOption}/>
+  if (orderShouldBeSend) {
+    const totalCost = formatPrice(calculateTotal(tripCost, options));
+    const payload = {
+      ...options,
+      totalCost,
+      tripDays,
+      tripName,
+      tripCountry,
+    };
+    const url = settings.db.url + '/' + settings.db.endpoint.orders;
+    const fetchOptions = {
+      cache: 'no-cache',
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(payload),
+    };
+    fetch(url, fetchOptions)
+      .then(function (response) {
+        return response.json();
+      }).then(function (parsedResponse) {
+        const orderSummary = [];
+        for(let item in parsedResponse){
+          orderSummary.push(`${item}: ${parsedResponse[item]}`);
+        }
+        alert('Order was sent, thank you!');
+      });
+  } else {
+    alert('Fill in all required fields!!!');
+  }
+};
+
+const OrderForm = (props) => {
+  const { options, setOrderOption, tripCost, tripDays, tripName, tripCountry } = props;
+  return (
+    <Row>
+      {pricing.map(option => (
+        <Col md={4} key={option.id}>
+          <OrderOption
+            currentValue={options[option.id]}
+            {...option}
+            setOrderOption={setOrderOption}
+          />
+        </Col>
+      ))}
+      <Col xs={12}>
+        <OrderSummary {...props} />
+        <Button onClick={() => sendOrder(
+          options, tripCost, tripDays, tripName, tripCountry
+        )}>Order now!</Button>
       </Col>
-    ))}
-
-    <Col xs={12}>
-      <OrderSummary tripCost={tripCost} options={options}/>
-    </Col>
-  </Row>
-);
+    </Row>
+  );
+};
 
 OrderForm.propTypes = {
-  tripCost: PropTypes.string,
   options: PropTypes.object,
   setOrderOption: PropTypes.func,
+  tripCost: PropTypes.string,
+  tripDays: PropTypes.number,
+  tripName: PropTypes.string,
+  tripCountry: PropTypes.string,
 };
 
 export default OrderForm;
--- src/components/views/Trip/Trip.js
+++ src/components/views/Trip/Trip.js
@@ -71,7 +71,7 @@ const Trip = ({error, name, image, cost, days, description, country, intro}) => 
         <Row>
           <Col xs={12}>
             <PageTitle text='Trip options' />
-            <OrderForm tripCost={cost} />
+            <OrderForm tripCost={cost} tripDays={days} tripName={name} tripCountry={country.alpha2Code}/>
           </Col>
         </Row>
       </Grid>
